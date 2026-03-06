1a20dc7b820335dccdacef816aea671599ec0192
Sebastian <sebastian.portuondo@lander.edu>
2026-03-06 12:58:02 -0500

    hardhat config changes for base sepolia

--- packages/hardhat/hardhat.config.js
+++ packages/hardhat/hardhat.config.js
@@ -1,9 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.33",
   gasReporter: {
     enabled: true,
+  },
+  networks: {
+    "base-sepolia": {
+      url: process.env.BASE_SEPOLIA_RPC_URL || "https://sepolia.base.org",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
   },
 };

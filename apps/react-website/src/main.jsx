908248b980e4f21f229308bc333789ede66e5046
LordLucas2851 <lucas.shaver@lander.edu>
2026-03-19 14:51:36 -0400

    Update main.jsx

--- apps/react-website/src/main.jsx
+++ apps/react-website/src/main.jsx
@@ -7,6 +7,17 @@ import { Toaster } from "react-hot-toast";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-    <Toaster position="top-right" />
+    <Toaster
+      position="bottom-center"
+      toastOptions={{
+        duration: 3000,
+        style: {
+          background: "#1f2937",
+          color: "#fff",
+          padding: "12px 16px",
+          borderRadius: "10px",
+        },
+      }}
+    />
   </React.StrictMode>
 );
